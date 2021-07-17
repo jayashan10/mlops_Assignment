9e1f0a56e9aae0fdaadae45e90d856ad4f927666
jayashan10 <rsjaya1999@gmail.com>
2021-07-18 02:40:24 +0530

    Random forest Classifier

--- src/expt.py
+++ src/expt.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, accuracy_score
 import pickle
 import json
+from sklearn.ensemble import RandomForestClassifier
 
 with dvc.api.open(repo="https://github.com/jayashan10/mlops_Assignment", path="data/creditcard.csv", mode="r") as fd:
     print("inside the with ")
@@ -15,16 +16,18 @@ df_train.to_csv('../data/processed/train.csv')
 df_test.to_csv('../data/processed/test.csv')
 
 
-tree_classifier = DecisionTreeClassifier(criterion="entropy")
-tree_classifier.fit(df_train.drop(columns='Class'), df_train['Class'])
-
-y_test_pred = tree_classifier.predict(df_test.drop(columns='Class'))
+#tree_classifier = DecisionTreeClassifier(criterion="entropy")
+forest_classifier = RandomForestClassifier()
+#tree_classifier.fit(df_train.drop(columns='Class'), df_train['Class'])
+forest_classifier.fit (df_train.drop(columns = 'Class'), df_train['Class'])
+#y_test_pred = tree_classifier.predict(df_test.drop(columns='Class'))
+y_test_pred = forest_classifier.predict(df_test.drop(columns='Class'))
 
 print(f"f1 score is: {f1_score(df_test['Class'], y_test_pred)}")
 print(f"the accuracy score is: {accuracy_score(df_test['Class'], y_test_pred)}")
 
 filename = '../models/tree_classifier.sav'
-pickle.dump(tree_classifier, open(filename, "wb" ))
+pickle.dump(forest_classifier, open(filename, "wb" ))
 
 
 my_dict = {}
